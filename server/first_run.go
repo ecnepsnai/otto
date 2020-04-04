e6ae7a9e9f90c165d7a13b4e4de25a330be3b007
Ian Spence <ian@ecnepsnai.com>
2020-04-04 15:24:17 -0700

    Create default group

--- server/first_run.go
+++ server/first_run.go
@@ -1,24 +1,44 @@
 package server
 
-import "os"
-
 var defaultUser = newUserParameters{
 	Username: "admin",
 	Email:    "admin@localhost",
 	Password: "admin",
 }
 
-func checkFirstRun() {
+var defaultGroup = newGroupParameters{
+	Name: "Otto Clients",
+}
+
+func isFirstRun() bool {
+	numberOfUsers := 0
+	numberOfGroups := 0
+
 	users, err := UserStore.AllUsers()
 	if err != nil {
 		panic(err)
 	}
-	if len(users) > 0 {
+	numberOfUsers = len(users)
+
+	groups, err := GroupStore.AllGroups()
+	if err != nil {
+		panic(err)
+	}
+	numberOfGroups = len(groups)
+
+	return numberOfUsers == 0 && numberOfGroups == 0
+}
+
+func checkFirstRun() {
+	if !isFirstRun() {
 		return
 	}
 
 	if _, err := UserStore.NewUser(defaultUser); err != nil {
-		log.Error("Unable to make default user: %s", err.Message)
-		os.Exit(1)
+		log.Fatal("Unable to make default user: %s", err.Message)
+	}
+
+	if _, err := GroupStore.NewGroup(defaultGroup); err != nil {
+		log.Fatal("Unable to make default group: %s", err.Message)
 	}
 }
