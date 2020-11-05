c451c9f29a26a6f8b10d69ac25680b5b9989c844
Ian Spence <ian@ecnepsnai.com>
2020-11-04 18:09:05 -0800

    Improve navbar

--- static/src/components/Nav.tsx
+++ static/src/components/Nav.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { Link } from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import { Icon } from './Icon';
 import { StateManager } from '../services/StateManager';
 import { Style } from './Style';
@@ -7,41 +7,7 @@ import { UserManager } from '../pages/options/OptionsUsers';
 import { API } from '../services/API';
 import '../../css/nav.scss';
 
-interface NavProps {}
-interface NavState {
-    active: {[id: string]: boolean};
-}
-export class Nav extends React.Component<NavProps, NavState> {
-    constructor(props: NavProps) {
-        super(props);
-        this.state = {
-            active: {
-                "hosts": false,
-                "groups": false,
-                "scripts": false,
-                "schedules": false,
-                "options": false,
-            },
-        };
-    }
-    componentDidMount(): void {
-        this.updateNavClass();
-
-        document.body.addEventListener('click', () => {
-            setTimeout(() => { this.updateNavClass(); }, 10);
-        });
-    }
-    private updateNavClass = () => {
-        this.setState(state => {
-            state.active.hosts = location.pathname.indexOf('/hosts') > -1;
-            state.active.groups = location.pathname.indexOf('/groups') > -1;
-            state.active.scripts = location.pathname.indexOf('/scripts') > -1;
-            state.active.schedules = location.pathname.indexOf('/schedules') > -1;
-            state.active.options = location.pathname.indexOf('/options') > -1;
-            return state;
-        });
-    }
-
+export class Nav extends React.Component<{}, {}> {
     private editUserClick = () => {
         UserManager.EditCurrentUser().then(() => { StateManager.Refresh(); });
     }
@@ -69,11 +35,11 @@ export class Nav extends React.Component<NavProps, NavState> {
                         </button>
                         <div className="collapse navbar-collapse justify-content-between" id="navbarNav">
                             <ul className="navbar-nav">
-                                <NavItem link="/hosts" active={this.state.active.hosts} icon={<Icon.Desktop />} label="Hosts" />
-                                <NavItem link="/groups" active={this.state.active.groups} icon={<Icon.LayerGroup />} label="Groups" />
-                                <NavItem link="/scripts" active={this.state.active.scripts} icon={<Icon.Scroll />} label="Scripts" />
-                                <NavItem link="/schedules" active={this.state.active.schedules} icon={<Icon.Calendar />} label="Schedules" />
-                                <NavItem link="/options" active={this.state.active.options} icon={<Icon.Cog />} label="Options" />
+                                <NavItem link="/hosts" icon={<Icon.Desktop />} label="Hosts" />
+                                <NavItem link="/groups" icon={<Icon.LayerGroup />} label="Groups" />
+                                <NavItem link="/scripts" icon={<Icon.Scroll />} label="Scripts" />
+                                <NavItem link="/schedules" icon={<Icon.Calendar />} label="Schedules" />
+                                <NavItem link="/options" icon={<Icon.Cog />} label="Options" />
                             </ul>
                             <ul className="navbar-nav navbar-links">
                                 <li className="nav-item dropdown">
@@ -109,20 +75,19 @@ interface NavItemProps {
     link: string;
     icon: JSX.Element;
     label: string;
-    active: boolean;
 }
 class NavItem extends React.Component<NavItemProps, {}> {
+    constructor(props: NavItemProps) {
+        super(props);
+    }
+
     render(): JSX.Element {
-        let className = 'nav-link';
-        if (this.props.active) {
-            className += ' active';
-        }
         return (
-        <li className="nav-item">
-            <Link to={this.props.link} className={className} data-target=".navbar-collapse.show" data-toggle="collapse">
-                <Icon.Label icon={this.props.icon} label={this.props.label} />
-            </Link>
-        </li>
+            <NavLink to={this.props.link} className="nav-link" activeClassName="active">
+                <li className="nav-item" data-target=".navbar-collapse.show" data-toggle="collapse">
+                    <Icon.Label icon={this.props.icon} label={this.props.label} />
+                </li>
+            </NavLink>
         );
     }
 }
