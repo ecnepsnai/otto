3673c07c9a28395e8f98d2131f3ff6e2429775e5
Ian Spence <ian@ecnepsnai.com>
2020-11-10 18:01:02 -0800

    Better clipboard error handling

--- static/src/services/Clipboard.ts
+++ static/src/services/Clipboard.ts
@@ -1,12 +1,22 @@
 export class Clipboard {
+    private static promptCopy(text: string): Promise<void> {
+        return new Promise(resolve => {
+            prompt('Copy the following text', text);
+            resolve();
+        });
+    }
     public static setText(text: string): Promise<void> {
+        if (!navigator.clipboard) {
+            return Clipboard.promptCopy(text);
+        }
+
         return new Promise(resolve => {
             navigator.clipboard.writeText(text).then(() => {
                 resolve();
             }, () => {
-                console.warn('Error setting clipboard contents - falling back to prompt');
-                prompt('Copy the following text', text);
-                resolve();
+                return Clipboard.promptCopy(text);
+            }).catch(() => {
+                return Clipboard.promptCopy(text);
             });
         });
     }
