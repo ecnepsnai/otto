fadc6b21d6020275d00f78d571fe169b64654aee
Ian Spence <ian@ecnepsnai.com>
2023-07-23 15:30:36 -0700

    Fix release script

--- scripts/install_backend.sh
+++ scripts/install_backend.sh
@@ -3,7 +3,8 @@ set -e
 
 ROOT_PATH=$(realpath ../)
 LOGS_PATH=${ROOT_PATH}/logs
-OTTO_PATH=$(realpath ../otto)
+OTTO_BACKEND_PATH=$(realpath ../otto)
+OTTO_FRONTEND_PATH=$(realpath ../frontend)
 SCRIPTS_PATH=$(realpath .)
 COLOR_NC='\033[0m'
 COLOR_RED='\033[0;31m'
@@ -16,9 +17,9 @@ mkdir -p ${LOGS_PATH}
 echo -en "Building backend... "
 cd ${SCRIPTS_PATH}/codegen/
 cbgen -n server
-mv *.go ${OTTO_PATH}/server
-mv *.ts ${OTTO_PATH}/frontend/src/types
-cd ${OTTO_PATH}/
+mv -v *.go ${OTTO_BACKEND_PATH}/server >> ${LOG} 2>&1
+mv -v *.ts ${OTTO_FRONTEND_PATH}/src/types >> ${LOG} 2>&1
+cd ${OTTO_BACKEND_PATH}/
 go build -v ./... >> ${LOG} 2>&1
 go test -v ./... >> ${LOG} 2>&1
 echo -e "${COLOR_GREEN}Finished${COLOR_NC}"
